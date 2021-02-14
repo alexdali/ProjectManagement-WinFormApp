94ff16c7f52856b265f59bc7a54e432316ad688b
Alexeytas <alexdali@gmail.com>
2021-02-14 20:16:31 +0400

    init commit

---
+++ ProjectManamgementWinApp/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace ProjectManamgementWinApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonProjects = new System.Windows.Forms.Button();
+            this.buttonUsers = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.labelProjects = new System.Windows.Forms.Label();
+            this.labelTasks = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonProjects
+            // 
+            this.buttonProjects.Location = new System.Drawing.Point(30, 29);
+            this.buttonProjects.Name = "buttonProjects";
+            this.buttonProjects.Size = new System.Drawing.Size(75, 23);
+            this.buttonProjects.TabIndex = 1;
+            this.buttonProjects.Text = "Projects";
+            this.buttonProjects.UseVisualStyleBackColor = true;
+            this.buttonProjects.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // buttonUsers
+            // 
+            this.buttonUsers.Location = new System.Drawing.Point(30, 73);
+            this.buttonUsers.Name = "buttonUsers";
+            this.buttonUsers.Size = new System.Drawing.Size(75, 23);
+            this.buttonUsers.TabIndex = 1;
+            this.buttonUsers.Text = "Users";
+            this.buttonUsers.UseVisualStyleBackColor = true;
+            this.buttonUsers.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.buttonProjects);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonUsers);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tableLayoutPanel1);
+            this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.SplitContainer1_Panel2_Paint);
+            this.splitContainer1.Size = new System.Drawing.Size(778, 357);
+            this.splitContainer1.SplitterDistance = 132;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 49.81132F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.18868F));
+            this.tableLayoutPanel1.Controls.Add(this.labelProjects, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.labelTasks, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 337F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(642, 357);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // labelProjects
+            // 
+            this.labelProjects.AutoSize = true;
+            this.labelProjects.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelProjects.Location = new System.Drawing.Point(3, 0);
+            this.labelProjects.Name = "labelProjects";
+            this.labelProjects.Size = new System.Drawing.Size(313, 20);
+            this.labelProjects.TabIndex = 0;
+            this.labelProjects.Text = "Projects";
+            this.labelProjects.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelProjects.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // labelTasks
+            // 
+            this.labelTasks.AutoSize = true;
+            this.labelTasks.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelTasks.Location = new System.Drawing.Point(322, 0);
+            this.labelTasks.Name = "labelTasks";
+            this.labelTasks.Size = new System.Drawing.Size(317, 20);
+            this.labelTasks.TabIndex = 1;
+            this.labelTasks.Text = "Tasks";
+            this.labelTasks.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 357);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonProjects;
+        private System.Windows.Forms.Button buttonUsers;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label labelProjects;
+        private System.Windows.Forms.Label labelTasks;
+    }
+}
+
